5691d13cc0c0f085a57a3505ef67468c35a68ba6
Rafael Sangalli <rafael.sangalli@vtex.com.br>
2021-08-11 16:34:12 -0300

    Adds constants for node environment

--- src/utils/configuration.utils.ts
+++ src/utils/configuration.utils.ts
@@ -1,7 +1,10 @@
+import { ENVIRONMENT } from './configuration.constants'
+
 export default class ConfigurationUtils {
   public static isProd(): boolean {
     return !(
-      process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+      process.env.NODE_ENV === ENVIRONMENT.development ||
+      process.env.NODE_ENV === ENVIRONMENT.test
     )
   }
 }
